5b17924d8366a7e8283d77df285ccdc29bc46bd0
Anna Liskovets <anna.liskovets@Annas-MacBook-Pro.local>
2022-10-19 12:42:56 -0400

    week 1. Modules  1-12

---
+++ 01-node-tutorial/answers/03-modules.js
@@ -0,0 +1,10 @@
+// modules
+const names = require("./04-names.js");
+const sayHi = require("./05-utils.js");
+const data = require("./06-alternative-flavor.js");
+require("./07-mind-grenade");
+//console.log(data);
+
+//sayHi("susan");
+//sayHi(names.john);
+//sayHi(names.peter);
---
+++ 01-node-tutorial/answers/07-mind-grenade.js
@@ -0,0 +1,8 @@
+const num1 = 5;
+const num2 = 10;
+
+function addValues() {
+  console.log(`Added values ${num1 + num2}`);
+}
+
+addValues();
