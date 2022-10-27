0524702ec6fd229643c97488b65ad98f580dd6ae
Anna Liskovets <anna.liskovets@Annas-MacBook-Pro.local>
2022-10-26 23:55:09 -0400

    answers for lesson 2

--- 01-node-tutorial/answers/app.js
+++ 01-node-tutorial/answers/app.js
@@ -1,17 +1,13 @@
-const http = require('http');
-const server = http.createServer((req, res) => {
-  if (req.url === '/') {
-    res.end('Welcome to our home page');
-  }
-  // console.log(req);
-  //  res.write();
-  //  res.end();
-  else if (req.url === '/about') {
-    res.end('Welcome to our about page');
-  } else
-    res.end(`<h1>Oops!</h1> 
-  <p> We can't seem to find the page you're looking for </p>
-  <a href="/"> back home </a>`);
+const { createReadStream } = require('fs');
+
+const stream = createReadStream('./content/big.txt', {
+  highWaterMark: 90000,
+  encoding: 'utf8',
+});
+stream.on('data', (result) => {
+  console.log(result);
 });
 
-server.listen(5000);
+stream.on('error', (error) => {
+  console.log(error);
+});
