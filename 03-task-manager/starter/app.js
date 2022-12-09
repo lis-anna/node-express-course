b18c6a6ddda962cef97589e5c80905e3a14fc187
Anna Liskovets <anna.liskovets@Annas-MacBook-Pro.local>
2022-12-09 11:58:09 -0500

    Changes for week6

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -3,23 +3,20 @@ const app = express();
 const tasks = require('./routes/tasks');
 const connectDB = require('./db/connect');
 require('dotenv').config();
-//const MONGO_URI = process.env.MONGO_URI;
+const notFound = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
 //middleware
+
+app.use(express.static('./public'));
 app.use(express.json());
 
 //routes
-app.get('/hello', (req, res) => {
-  res.status(200).send('Task Manager App');
-});
 
 app.use('/api/v1/tasks', tasks);
-//app.get('/api/v1/tasks') - get all the tasks
-// app.post('/api/v1/tasks') - create a new task
-// app.get('/api/v1/tasks/:id') - get single task
-// app.patch('/api/v1/tasks/:id') - update task
-//app.delete('/api/v1/tasks/:id') - delete task
-
-const port = 5000;
+app.use(notFound);
+app.use(errorHandlerMiddleware);
+const port = process.env.PORT || 5000;
 
 const start = async () => {
   try {
